19a22d02b8af8b26094e289251b5760863a178dd
Chirag Maski <78970626+chiraghm4@users.noreply.github.com>
2023-07-19 18:00:07 +0530

    updated path of build

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/todos", todoRouter)
 app.use("/user", userRouter)
-app.use(express.static(path.join(__dirname, './frontend/todolist/build')))
+app.use(express.static(path.join(__dirname, './frontend/build')))
 
 // rest api
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/todolist/build/index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
 
 const PORT = process.env.PORT || 8000;
